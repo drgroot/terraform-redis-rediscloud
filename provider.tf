1b7196b7b9667310c86aecbf33cdaddf0546acf0
Yusuf Ali <ali@yusuf.email>
2026-05-15 12:36:26 -0400

    fix: creds

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "rediscloud" {
-    secret_key = data.vault_generic_secret.upstash.data["key"]
-    api_key    = data.vault_generic_secret.upstash.data["account-key"]
+    secret_key = data.vault_generic_secret.rediscloud.data["key"]
+    api_key    = data.vault_generic_secret.rediscloud.data["account-key"]
 }
 
 data "vault_generic_secret" "rediscloud" {
