f9d1368c5776693a5b265968c31797be5d3f15ae
Yusuf Ali <ali@yusuf.email>
2026-05-15 12:37:55 -0400

    a

--- outputs.tf
+++ outputs.tf
@@ -3,4 +3,6 @@ output "credentials" {
     hostname = split(":", rediscloud_essentials_database.database.public_endpoint)[0]
     url = "redis://default:${rediscloud_essentials_database.database.password}@${rediscloud_essentials_database.database.public_endpoint}"
   }
+
+  sensitive = true
 }
